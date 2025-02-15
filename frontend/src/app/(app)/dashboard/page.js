747f1adbc62cee34b418b97ba051480150b28486
Sugita Yamato <yamameen1216@icloud.com>
2025-02-16 02:45:24 +0900

    スライダーにfade適用

--- frontend/src/app/(app)/dashboard/page.js
+++ frontend/src/app/(app)/dashboard/page.js
@@ -2,10 +2,11 @@
 import Header from '@/app/(app)/Header'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination, Autoplay } from 'swiper/modules' 
+import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules' 
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import 'swiper/css/effect-fade';
 import { useState } from 'react'
 import MissionList from '@/components/MissionList'
 
@@ -41,15 +42,17 @@ const Dashboard = () => {
                             {/* 枠を作成 */}
                             <div className="border-4 border-gray-300 p-4 rounded-lg shadow-lg">
                                 <Swiper
-                                    modules={[Navigation, Pagination, Autoplay]}
+                                    modules={[Navigation, Pagination, Autoplay, EffectFade]}
                                     slidesPerView={1}
                                     pagination={{ clickable: true }}
                                     navigation
                                     loop={true}
                                     autoplay={{
-                                        delay: 3500,
+                                        delay: 4000,
                                         disableOnInteraction: false,
                                     }}
+                                    effect="fade"
+                                    speed={2000}
                                 >
                                     {images.map((src, index) => (
                                         <SwiperSlide key={index}>
@@ -59,30 +62,12 @@ const Dashboard = () => {
                                 </Swiper>
 
                                 {/* デイリーリーミッションの枠 */}
-                            <div className="mt-8 bg-gray-100 p-6 rounded-lg shadow-md">
-                                <h2 className="text-xl font-semibold text-gray-700">Dayly Mission</h2>
-                                <ul className="mt-4 space-y-4 text-gray-600">
-                                    {missions.map((mission, index) => (
-                                        <li key={index} className="flex items-start space-x-2">
-                                            <div className="flex-shrink-0 w-4 h-4 bg-blue-500 rounded-full mt-1"></div>
-                                            <div>
-                                                <p className="font-semibold">{mission.title}</p>
-                                                {/*<p className="text-sm">{mission.description}</p>*/}
-                                                {mission.completed ? (
-                                                    <p className="text-green-600 text-sm">Completed</p>
-                                                ) : (
-                                                    <button
-                                                        onClick={() => handleCompleteMission(index)}
-                                                        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded-md text-sm">
-                                                        完了
-                                                    </button>
-                                                )}
-                                            </div>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <MissionList />
+                                <div className="mt-8 bg-gray-100 p-6 rounded-lg shadow-md">
+                                    <h2 className="text-xl font-semibold text-gray-700">Dayly Mission</h2>
+                                    <ul className="mt-4 space-y-4 text-gray-600">
+                                        <MissionList />
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
